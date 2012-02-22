90c1d189c01472de6d657a08a4caf076857b09b3
Lareb Nawab <lareb.indore@gmail.com>
2012-02-22 14:55:40 +0530

    add new tile design, with 3d buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def formated_user_name(user)
+    return "" if user.nil?
+    return "#{user.email}" if user.first_name.nil?
+    return "#{user.first_name} #{user.last_name}".capitalize
+  end
+
 end
